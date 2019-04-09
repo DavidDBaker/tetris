5166a10119919cde44b8c78a9e1c7ae874e84db0
DavidDBaker <daviiddamiianbaker@gmail.com>
2019-04-09 12:41:14 -0700

    rotate function working

--- assets/js/index.js
+++ assets/js/index.js
@@ -78,6 +78,41 @@ function playerMove(dir){
   }
 }
 
+function playerRotate(dir){
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix,dir);
+  while (collision(arena,player)){
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length){
+      rotate(player.matrix, -dir);
+      player.pos = pos;
+      return;
+    }
+  }
+}
+
+function rotate(matrix,dir){
+  for(let y=0;y <matrix.length; ++y){
+    for (let x=0; x <y; ++x){
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+        matrix[y][x],
+        matrix[x][y],
+      ];
+    }
+  }
+  if (dir > 0){
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
+
+
 let dropCounter = 0; 
 let dropInterval = 1000;
 
@@ -111,6 +146,10 @@ document.addEventListener('keydown', event => {
     playerMove(+1);
   } else if (event.keyCode === 40){
     playerDrop();
+  } else if(event.keyCode === 81){
+    playerRotate(-1);
+  } else if(event.keyCode === 87){
+    playerRotate(1);
   }
 });
 
